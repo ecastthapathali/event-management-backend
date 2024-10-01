dd02e18b15a6cf300411191866a73d6040d7d851
UJJWAL <122596469+dahalujjwal@users.noreply.github.com>
2024-10-01 19:07:31 +0545

    JWT Auth is Used for Login & CRUD Of Each API is Created

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import  AbstractBaseUser , PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .manager import UserManager
 from django.contrib.auth.models import Group, Permission
 
-
-class UserData(AbstractBaseUser , PermissionsMixin):
+class UserData(AbstractBaseUser, PermissionsMixin):
     FACULTY_CHOICES = (
         ('BEI', 'Electronics, Communication, and Information Engineering'),
         ('BCT', 'Computer Engineering'),
@@ -20,10 +19,10 @@ class UserData(AbstractBaseUser , PermissionsMixin):
         ('BCHE', 'Chemical Engineering'),
     )
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=200,unique=True)
+    email = models.EmailField(max_length=200, unique=True)
     phone_number = models.CharField(max_length=10)
     campus_name = models.CharField(max_length=250)
-    faculty = models.CharField(max_length=10 , choices=FACULTY_CHOICES,default='BEI')
+    faculty = models.CharField(max_length=10, choices=FACULTY_CHOICES, default='BEI')
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -39,7 +38,7 @@ class UserData(AbstractBaseUser , PermissionsMixin):
     
     def __str__(self):
         return f"Name : {self.name} Email : {self.email}"
-    
+
 class EventModel(models.Model):
     organizer_id = models.ForeignKey(UserData, on_delete=models.CASCADE)
     event_name = models.CharField(max_length=500)
@@ -50,30 +49,28 @@ class EventModel(models.Model):
     
     def __str__(self):
         return self.event_name
-    
-
 
 class EventReg(models.Model):
-    event_id = models.ForeignKey(EventModel, on_delete=models.CASCADE)
-    participant_id = models.OneToOneField(UserData,on_delete=models.CASCADE)
+    event = models.ForeignKey(EventModel, on_delete=models.CASCADE)  
+    participant = models.ForeignKey(UserData, on_delete=models.CASCADE) 
     reg_date = models.DateTimeField(default=timezone.now)
-    
+
     def __str__(self):
-        return self.participant_id.name
-    
+        return f"{self.participant.name} registered for {self.event.event_name}"
+
 class Participants(models.Model):
-    event_reg = models.OneToOneField(EventReg,on_delete=models.CASCADE)
+    event_reg = models.OneToOneField(EventReg, on_delete=models.CASCADE)
     class_link = models.TextField()
     certificate_sent = models.BooleanField(default=False)
-    certificate_sent_on = models.DateTimeField(blank=True , null=True)
+    certificate_sent_on = models.DateTimeField(blank=True, null=True)
     
     def __str__(self):
-        return f" Name : {self.event_reg.participant_id.name} Event : {self.event_reg.event_id.event_name}"
-    
+        return f"Name: {self.event_reg.participant.name} Event: {self.event_reg.event.event_name}"
+
 class DigitalCertificate(models.Model):
-    cert_id = models.OneToOneField(Participants,on_delete=models.CASCADE)
-    sent_on = models.DateTimeField(blank=True,null=True)
+    cert_id = models.OneToOneField(Participants, on_delete=models.CASCADE)
+    sent_on = models.DateTimeField(blank=True, null=True)
     certificate_url = models.CharField(max_length=300)
     
     def __str__(self):
-        return self.cert_id.event_reg.participant_id.name
+        return self.cert_id.event_reg.participant.name
